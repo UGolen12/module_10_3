b6904a9cc3fcdf564050d7f8a336fccf77f14b29
UGolen12 <emerdielle@gmail.com>
2024-12-21 23:29:33 +0700

    Update module_10_3.py

--- module_10_3.py
+++ module_10_3.py
@@ -12,12 +12,12 @@ class Bank(Thread):
         self.lock = Lock()
 
     def deposit(self):
-        for i in range(100):
-            if self.balance >= 500 and self.lock.locked():
-                self.lock.release()
+        for i in range(100):            
             a = randint(50, 500)
             self.balance += a
             print(f'Пополнение: {a}. Баланс: {self.balance}')
+            if self.balance >= 500 and self.lock.locked():
+                self.lock.release()
             sleep(0.001)
 
     def take(self):
