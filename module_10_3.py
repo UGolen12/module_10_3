e2e1b38aeccf771f33e8692d9de0fca8e4c19362
UGolen12 <emerdielle@gmail.com>
2024-12-22 00:32:18 +0700

    Update module_10_3.py

--- module_10_3.py
+++ module_10_3.py
@@ -1,10 +1,9 @@
-import threading
 from threading import Thread, Lock
 from time import sleep
 from random import randint
 
 
-class Bank(Thread):
+class Bank:
 
     def __init__(self):
         super().__init__()
@@ -12,13 +11,13 @@ class Bank(Thread):
         self.lock = Lock()
 
     def deposit(self):
-        for i in range(100):            
+        for i in range(100):
             a = randint(50, 500)
             self.balance += a
             print(f'Пополнение: {a}. Баланс: {self.balance}')
             if self.balance >= 500 and self.lock.locked():
                 self.lock.release()
-            sleep(0.001)
+            sleep(0.01)
 
     def take(self):
         for i in range(100):
@@ -30,12 +29,12 @@ class Bank(Thread):
             else:
                 print(f'Запрос отклонён, недостаточно средств')
                 self.lock.acquire()
-            sleep(0.001)
+            sleep(0.01)
 
 
 bk = Bank()
-th1 = threading.Thread(target=Bank.deposit, args=(bk,))
-th2 = threading.Thread(target=Bank.take, args=(bk,))
+th1 = Thread(target=Bank.deposit, args=(bk,))
+th2 = Thread(target=Bank.take, args=(bk,))
 
 th1.start()
 th2.start()
